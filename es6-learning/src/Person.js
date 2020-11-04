9b9da76277ef4d00603199087d205d9c1069245f
Vankum100 <kumkarana04@gmail.com>
2020-11-04 14:00:33 +0300

    completed babel demo

---
+++ es6-learning/src/Person.js
@@ -0,0 +1,18 @@
+
+// Babel Helps us `output` this `src` es2015 code in vanilla js 
+// transformations are only possible with help of
+// babel-preset-es2015 package
+// set up the presets in `.babelrc` file
+
+
+class Person {
+    constructor(name){
+        this.name = name;
+    }
+
+    greet() {
+        return this.name + ' says hello.'; 
+    }
+}
+
+console.log(new Person('jeffrey').greet());
